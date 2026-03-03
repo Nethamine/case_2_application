5cd93b97a52327c87b2261cddd9c5158d88db1ab
Nethamine <nathan.phang123@gmail.com>
2026-03-03 13:22:39 +0000

    Test

--- Dashboard.py
+++ Dashboard.py
@@ -2,3 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.title("League data vergeijking")
+
+tab1, tab2, tab3 = st.tabs(["Tab 1, Tab 2, Tab 3"])
+
+st.slider(min_value='0', max_value='2', step=1)
