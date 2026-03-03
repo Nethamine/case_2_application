c62df3da0889b90da0a6c52adfa09ad765c78c45
Nethamine <nathan.phang123@gmail.com>
2026-03-03 13:24:01 +0000

    Update op test

--- Dashboard.py
+++ Dashboard.py
@@ -7,3 +7,14 @@ st.title("League data vergeijking")
 tab1, tab2, tab3 = st.tabs(["Tab 1, Tab 2, Tab 3"])
 
 st.slider(min_value='0', max_value='2', step=1)
+
+with tab1: 
+    st.write('Tab 1')
+
+with tab2:
+    st.write('Tab 2')
+
+with tab3:
+    st.write('Tab 3')
+
+    
