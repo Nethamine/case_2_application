8f1bd3b575703364fdfa315c5fcc796bc6d7a3a8
Nethamine <nathan.phang123@gmail.com>
2026-03-03 13:29:12 +0000

    Verandering 3.0

--- Dashboard.py
+++ Dashboard.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title("League data vergeijking")
 
-tab1, tab2, tab3 = st.tabs(["Tab 1, Tab 2, Tab 3"])
+tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
 
 st.slider(min_value='0', max_value='2', step=1)
 
@@ -17,4 +17,3 @@ with tab2:
 with tab3:
     st.write('Tab 3')
 
-    
