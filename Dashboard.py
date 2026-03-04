57c24f4dd687764cdf9dcec8e23887eb1ac48aaa
Thombutter <thom.butter@hva.nl>
2026-03-04 10:34:21 +0000

    deleted req + reverting changes made

--- Dashboard.py
+++ Dashboard.py
@@ -1,23 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from plot import save_winrate_plot, save_counterpick_heatmap
+
 st.title("League data vergeijking")
-# laad per tier
-df_chal = pd.read_csv("challenger_matches_useful.csv")
-df_master = pd.read_csv("master_matches_useful.csv")
-df_gm = pd.read_csv("grandmaster_matches_useful.csv")
 
 tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
 
-with tab1:
-    path = save_winrate_plot(df_chal, 'Challenger')
-    st.image(path)
+with tab1: 
+    st.write('Tab 1')
 
 with tab2:
-    path = save_counterpick_heatmap(df_chall, 'Challenger', role)
-    if path:
-        st.image(path)
+    st.write('Tab 22')
 
 with tab3:
     st.write('Tab 3')
