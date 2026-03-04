6c565fc5fdbf3c095ea4b6a47001aab908507917
Nethamine <nathan.phang123@gmail.com>
2026-03-04 11:45:46 +0000

    Verandering met data inladen

--- Dashboard.py
+++ Dashboard.py
@@ -1,10 +1,27 @@
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+
+df_chal = pd.read_csv("challenger_matches_useful.csv")
+df_gm = pd.read_csv("grandmaster_matches_useful.csv")
+df_m = pd.read_csv("master_matches_useful.csv")
+
+df_all = pd.concat([df_chal,df_gm, df_m], ignore_index=True)
+
+# CODE VANAF HIER
+
+
 
 st.title("League data vergeijking")
 
-tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
+
+with st.sidebar:
+    st.header("Filters", divider==True)
+    selected_atribute = st.selectbox("Kies Rank/Tier", df_all['tier'].unique())
+
+tab1, tab2, tab3 = st.tabs(["Winrate vs Champ ", "Rank vs Champ gespeelt", "Tab 3"])
 
 with tab1: 
     st.write('Tab 1')
